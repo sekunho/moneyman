fe8d0b229a12d571d1a030912185f444430d756a
sekun <software@sekun.net>
2025-02-14 00:10:18 +0800

    chore: Bump nixpkgs and refactor to crane (#20)
    * chore: Bump nixpkgs and fenix
    
    * refactor: naersk -> crane
    
    * chore(ci): Change nix installer, and bump checkout version
    
    * chore(ci): Run tests regularly

--- flake.nix
+++ flake.nix
@@ -1,91 +1,91 @@
 {
   inputs = {
-    fenix.url = "github:nix-community/fenix";
-    naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    crane.url = "github:ipetkov/crane";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
   };
 
-  outputs = { self, fenix, naersk, nixpkgs }:
+  outputs = { self, nixpkgs, crane, fenix }:
     let
       system = "x86_64-linux";
       pkgs = (import nixpkgs) { inherit system; };
-      fenix' = fenix.packages.${system};
 
-      toolchain = with fenix'; combine [
-        stable.rustc
-        stable.cargo
-        targets.x86_64-unknown-linux-musl.stable.rust-std
-      ];
+      craneLib = (crane.mkLib pkgs).overrideToolchain
+        fenix.packages.${system}.stable.toolchain;
 
-      naersk' = naersk.lib.${system}.override {
-        cargo = toolchain;
-        rustc = toolchain;
+      src = pkgs.lib.cleanSourceWith {
+        src = ./.;
+
+        filter = path: type:
+          (craneLib.filterCargoSources path type)
+        ;
       };
 
-      pname = "moneyman_cli";
-      version = "0.1.2";
-    in {
+
+      commonArgs = {
+        inherit src;
+        version = "0.1.2";
+        strictDeps = true;
+        pname = "moneyman";
+        name = "moneyman_cli";
+        buildInputs = [ ];
+        nativeBuildInputs = [ ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      moneyman = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        doCheck = false;
+        CARGO_PROFILE = "release";
+      });
+
+    in
+    {
       packages = {
         x86_64-linux = rec {
+          inherit moneyman;
           default = moneyman;
-
-          moneyman = naersk'.buildPackage {
-            inherit pname version;
-
-            src = ./.;
-            doCheck = false;
-            nativeBuildInputs = with pkgs; [ openssl pkg-config ];
-          };
-
-          # FIXME: This is broken atm. I wouldn't use this cause it won't even
-          # compile due to it trying to dynamically link something musl doesn't
-          # apparently have.
-          moneyman-static = naersk'.buildPackage {
-            inherit pname version;
-
-            src = ./.;
-            doCheck = false;
-
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-
-            nativeBuildInputs = with pkgs; [
-              pkgsStatic.stdenv.cc
-            ];
-
-            buildInputs = [];
-          };
         };
       };
 
       devShells.${system} = {
-        ci = pkgs.mkShell {
-          buildInputs = [
-            fenix'.stable.rustc
-            fenix'.stable.cargo
-            fenix'.stable.clippy
-            fenix'.stable.rustfmt
-          ];
+        ci = craneLib.devShell {
+          packages = [];
         };
 
-        default = let
-          rustPackages = with fenix'.stable; [
-            rustc
-            cargo
-            clippy
-            rustfmt
-            pkgs.rust-analyzer
-            pkgs.cargo-flamegraph
-          ];
+        default =
+          let
+            rustPackages = [
+              pkgs.cargo-flamegraph
+            ];
 
-          nixPackages = with pkgs; [ nil ];
+            nixPackages = with pkgs; [
+              nil
+              nixpkgs-fmt
+            ];
 
-          misc = with pkgs; [
-            pkgsStatic.sqlite
-          ];
-        in pkgs.mkShell {
-          buildInputs = rustPackages ++ nixPackages ++ misc;
-        };
+            misc = with pkgs; [
+              pkgsStatic.sqlite
+              git
+            ];
+
+            darwinPackages = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+              libiconv
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.CoreServices
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ]);
+          in
+          craneLib.devShell {
+            buildInputs = rustPackages ++ nixPackages ++ misc ++ darwinPackages;
+          };
       };
     };
 }
