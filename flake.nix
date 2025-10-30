54ff84798e12c8974188bac2dbaf9dfb024f83ba
sekun <software@sekun.net>
2025-10-30 07:43:04 +0100

    refactor: Drop thiserror (#24)
    * refactor: Drop thiserror
    
    * fix(nix): Temporary fix on new 1.90 linker
    
    * fix: appease clippy

--- flake.nix
+++ flake.nix
@@ -1,91 +1,85 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     crane.url = "github:ipetkov/crane";
 
     fenix = {
-      url = "github:nix-community/fenix";
+      # TODO: Remove when https://github.com/nix-community/fenix/issues/206 is OK
+      url = "github:marienz/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.rust-analyzer-src.follows = "";
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix }:
-    let
-      system = "x86_64-linux";
-      pkgs = (import nixpkgs) { inherit system; };
+  outputs = inputs@{ self, nixpkgs, flake-parts, crane, fenix }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      imports = [ ];
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain
-        fenix.packages.${system}.stable.toolchain;
+      perSystem = { config, system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-      src = pkgs.lib.cleanSourceWith {
-        src = ./.;
+          craneLib = (crane.mkLib pkgs).overrideToolchain
+            fenix.packages.${system}.stable.toolchain;
 
-        filter = path: type:
-          (craneLib.filterCargoSources path type)
-        ;
-      };
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
 
-
-      commonArgs = {
-        inherit src;
-        version = "0.1.2";
-        strictDeps = true;
-        pname = "moneyman";
-        name = "moneyman_cli";
-        buildInputs = [ ];
-        nativeBuildInputs = [ ];
-      };
-
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-      moneyman = craneLib.buildPackage (commonArgs // {
-        inherit cargoArtifacts;
-        doCheck = false;
-        CARGO_PROFILE = "release";
-      });
-
-    in
-    {
-      packages = {
-        x86_64-linux = rec {
-          inherit moneyman;
-          default = moneyman;
-        };
-      };
-
-      devShells.${system} = {
-        ci = craneLib.devShell {
-          packages = [];
-        };
-
-        default =
-          let
-            rustPackages = [
-              pkgs.cargo-flamegraph
-            ];
-
-            nixPackages = with pkgs; [
-              nil
-              nixpkgs-fmt
-            ];
-
-            misc = with pkgs; [
-              pkgsStatic.sqlite
-              git
-            ];
-
-            darwinPackages = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
-              libiconv
-              darwin.apple_sdk.frameworks.CoreFoundation
-              darwin.apple_sdk.frameworks.CoreServices
-              darwin.apple_sdk.frameworks.Security
-              darwin.apple_sdk.frameworks.SystemConfiguration
-            ]);
-          in
-          craneLib.devShell {
-            buildInputs = rustPackages ++ nixPackages ++ misc ++ darwinPackages;
+            filter = path: type:
+              (craneLib.filterCargoSources path type)
+            ;
           };
-      };
+
+          commonArgs = {
+            inherit src;
+            version = "0.1.2";
+            strictDeps = true;
+            pname = "moneyman";
+            name = "moneyman_cli";
+            buildInputs = [ ];
+            nativeBuildInputs = [ ];
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          moneyman = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            doCheck = false;
+            CARGO_PROFILE = "release";
+          });
+        in {
+          packages = rec {
+            inherit moneyman;
+            default = moneyman;
+          };
+
+          devShells = {
+            ci = craneLib.devShell {
+              packages = [ ];
+            };
+
+            default =
+              let
+                rustPackages = [
+                  pkgs.cargo-flamegraph
+                ];
+
+                nixPackages = with pkgs; [
+                  nil
+                  nixpkgs-fmt
+                ];
+
+                misc = with pkgs; [
+                  pkgsStatic.sqlite
+                  git
+                ];
+              in
+              craneLib.devShell {
+                buildInputs = rustPackages ++ nixPackages ++ misc;
+              };
+          };
+        };
     };
 }
